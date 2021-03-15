8e4f05e3013fa50fcf5512d025182ea5c209e342
Srishti Kohli <srishtik94@gmail.com>
2021-03-15 09:54:13 +0530

    Entity extractor bert training testing added

--- Morph/bertEntity/dataset.py
+++ Morph/bertEntity/dataset.py
@@ -3,11 +3,8 @@ import torch
 
 
 class EntityDataset:
-    def __init__(self, texts, pos, tags):
-        # texts: [["hi", ",", "my", "name", "is", "rahul"], ["hello".....]]
-        # pos/tags: [[1 2 3 4 1 5], [....].....]]
+    def __init__(self, texts, tags):
         self.texts = texts
-        self.pos = pos
         self.tags = tags
     
     def __len__(self):
@@ -15,11 +12,9 @@ class EntityDataset:
     
     def __getitem__(self, item):
         text = self.texts[item]
-        pos = self.pos[item]
         tags = self.tags[item]
 
         ids = []
-        target_pos = []
         target_tag =[]
 
         for i, s in enumerate(text):
@@ -29,15 +24,12 @@ class EntityDataset:
             )
             input_len = len(inputs)
             ids.extend(inputs)
-            target_pos.extend([pos[i]] * input_len)
             target_tag.extend([tags[i]] * input_len)
 
         ids = ids[:config.MAX_LEN - 2]
-        target_pos = target_pos[:config.MAX_LEN - 2]
         target_tag = target_tag[:config.MAX_LEN - 2]
 
         ids = [101] + ids + [102]
-        target_pos = [0] + target_pos + [0]
         target_tag = [0] + target_tag + [0]
 
         mask = [1] * len(ids)
@@ -48,13 +40,11 @@ class EntityDataset:
         ids = ids + ([0] * padding_len)
         mask = mask + ([0] * padding_len)
         token_type_ids = token_type_ids + ([0] * padding_len)
-        target_pos = target_pos + ([0] * padding_len)
         target_tag = target_tag + ([0] * padding_len)
 
         return {
             "ids": torch.tensor(ids, dtype=torch.long),
             "mask": torch.tensor(mask, dtype=torch.long),
             "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
-            "target_pos": torch.tensor(target_pos, dtype=torch.long),
             "target_tag": torch.tensor(target_tag, dtype=torch.long),
         }
