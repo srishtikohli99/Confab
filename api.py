de42137e47a732a2a367cbe5427d414852248df5
bajajra <rbajaj1998official@gmail.com>
2021-04-10 18:01:15 +0530

    REST service api file added

---
+++ api.py
@@ -0,0 +1,35 @@
+from bureau import classifier
+import os
+import json
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+with open(os.path.join(os.getcwd(),"config.json")) as f:
+    config = json.load(f)
+
+if config["entityExtractor"] == "flair":
+    from Morph.flairEntity import predict as pred_flair
+else:
+    from Morph.bertEntity import predict
+
+@app.get("/bot/{utterance}")
+def bot_response(utterance: str):
+    phrase = utterance
+    if config["entity"]:
+        if config["entityExtractor"] == "flair":
+            entities = pred_flair.FlairPredict(phrase)
+        else:
+            entities = entity_extraction_bert(phrase)
+    else:
+        entities = None
+
+
+    response_text = classifier.intent_classify(phrase, entities)
+    return {"response_text": response_text}
+
+def entity_extraction_bert(phrase):
+
+    entities = predict.predictor(phrase)
+    return entities
